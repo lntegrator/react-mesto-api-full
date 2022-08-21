b3d6a6be891ef33d2660900137dc663301bbc68e
Vasiliy Yanchuk <yanchuk8kz@yandex.ru>
2022-08-21 18:16:46 +0600

    fck

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -108,6 +108,7 @@ module.exports.login = (req, res, next) => {
 
   return User.findUserbyCredentials(email, password)
     .then((user) => {
+      console.log(user._id);
       const token = jwt.sign(
         { _id: user._id },
         NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key',
